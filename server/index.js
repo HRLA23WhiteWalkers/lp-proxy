1e24524ec9aecc62fd341ed8122ee5f63b7b2abc
LesleyPark <lesparkley@gmail.com>
2018-08-01 09:07:41 -0700

    Build proxy server
    Renders photo module that was built out; still need to incorporate other modules from group

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require ('express');
+const path = require('path');
+const parser = require('body-parser');
+const helmet = require('helmet');
+const request = require('request');
 
-var app = express();
+const proxy = express();
+const port = 8080;
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+proxy.use(helmet());
+proxy.use(parser.json());
+proxy.use(parser.urlencoded({extended: true}));
+proxy.use(express.static(path.join(__dirname, '../')));
 
-app.listen(8080, function() {
-  console.log('listening on port 3000!');
-});
+// proxy.get('/bundle.js/:3000', (req, res) => {
+//   request('http://localhost:3000/bundle.js', (error, response, body) => {
+//     console.log(typeof body)
+//     res.status(200).send(body);
+//   })
+// })
+
+proxy.use('/api/rooms/1', (req, res) => {
+  request('http://localhost:3000/api/rooms/1', (error, response, body) => {
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+
+proxy.listen(port, () => console.log('Connected on port ' + port + ' ^____^b'));
