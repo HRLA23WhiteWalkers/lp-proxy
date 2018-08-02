0b026e6b2ad035a59c1b6dcc0486059635520c9e
LesleyPark <lesparkley@gmail.com>
2018-08-02 16:34:21 -0700

    Integrate Jun and Blake's modules
    All modules integrated! Next steps: deployment

--- server/index.js
+++ server/index.js
@@ -12,15 +12,22 @@ proxy.use(parser.json());
 proxy.use(parser.urlencoded({extended: true}));
 proxy.use(express.static(path.join(__dirname, '../')));
 
-// proxy.get('/bundle.js/:3000', (req, res) => {
-//   request('http://localhost:3000/bundle.js', (error, response, body) => {
-//     console.log(typeof body)
-//     res.status(200).send(body);
-//   })
-// })
-
 proxy.use('/api/rooms/1', (req, res) => {
   request('http://localhost:3000/api/rooms/1', (error, response, body) => {
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+proxy.use('/api/pageDetails/data', (req, res) => {
+  request('http://localhost:3018/api/pageDetails/data', (error, response, body) => {
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+proxy.use('/api/amenities', (req, res) => {
+  request('http://localhost:3012/api/amenities', (error, response, body) => {
     if(response.statusCode === 200) {
       res.status(200).send(body);
     }
