05baf8603a694a9a920c0c0804d2e1d30006a29c
LesleyPark <lesparkley@gmail.com>
2018-08-04 13:49:44 -0700

    Modified proxy.use statements to serve assets from EC2 instead of localhost

--- server/index.js
+++ server/index.js
@@ -13,25 +13,25 @@ proxy.use(parser.urlencoded({extended: true}));
 proxy.use(express.static(path.join(__dirname, '../')));
 
 proxy.use('/api/rooms/1', (req, res) => {
-  request('http://localhost:3000/api/rooms/1', (error, response, body) => {
+  request('http://ec2-54-67-115-216.us-west-1.compute.amazonaws.com:9000//api/rooms/1', (error, response, body) => {
     if(response.statusCode === 200) {
       res.status(200).send(body);
     }
   })
 })
-proxy.use('/api/pageDetails/data', (req, res) => {
-  request('http://localhost:3018/api/pageDetails/data', (error, response, body) => {
-    if(response.statusCode === 200) {
-      res.status(200).send(body);
-    }
-  })
-})
-proxy.use('/api/amenities', (req, res) => {
-  request('http://localhost:3012/api/amenities', (error, response, body) => {
-    if(response.statusCode === 200) {
-      res.status(200).send(body);
-    }
-  })
-})
+// proxy.use('/api/pageDetails/data', (req, res) => {
+//   request('http://localhost:3018/api/pageDetails/data', (error, response, body) => {
+//     if(response.statusCode === 200) {
+//       res.status(200).send(body);
+//     }
+//   })
+// })
+// proxy.use('/api/amenities', (req, res) => {
+//   request('http://localhost:3012/api/amenities', (error, response, body) => {
+//     if(response.statusCode === 200) {
+//       res.status(200).send(body);
+//     }
+//   })
+// })
 
 proxy.listen(port, () => console.log('Connected on port ' + port + ' ^____^b'));
